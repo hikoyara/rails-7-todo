89bf07bc0c27ca8d813c09e5e4875e28f0ed0a62
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-02-15 13:55:13 +0900

    Create Task_Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
 
-  root 'tasks#index'
+    root 'tasks#index'
 
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+    get 'signup', to: 'users#new'
+    post 'signup', to: 'users#create'
 
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+
+    resources :users, only: [] do
+        resources :tasks, only: :index
+    end
+    resources :tasks, only: :create
 end
