0cb9b7456b142f893ec56b6d6c2a12e0f45bacf4
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-02-15 10:32:56 +0900

    Create LoginBackend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,14 @@ class SessionsController < ApplicationController
     def new; 
     end
   
-    def create; 
+    def create
+        user = User.find_by(account_id: params[:session][:account_id])
+        if user&.authenticate(params[:session][:password])
+            login user
+            redirect_to root_path
+        else
+            flash.now[:alert] = 'アカウントID、またはパスワードが間違っています。'
+            render :new
+        end
     end
 end
