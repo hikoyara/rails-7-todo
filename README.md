ee52176bf8f0e8d8f0a26c05243dcc4a956b333a
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-02-15 11:35:53 +0900

    Add Login_Required

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 -   docker-compose run web rails db:create && docker-compose up
 
-/\*
 発生している不具合
 
 -   サインアップ後の画面切り替えができない、バリデーションのエラー文が表示できない（layouts を削除することで解決できる）
-    \*/
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
 class TasksController < ApplicationController
-    def index;
+    before_action :login_required
+
+    def index; end
+
+    private
+
+    def login_required
+        redirect_to login_path unless logged_in?
     end
 end
